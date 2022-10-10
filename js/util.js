09f90c8b361722de7ab591537b27ede22b2fbdac
Vanesflat <ptptcptptc@mail.ru>
2022-10-10 13:18:05 +0700

    Deleted createCounter. Refresh getRandomIndex to getRandomArrayElement

--- js/util.js
+++ js/util.js
@@ -16,7 +16,7 @@ function getRandomFractNumber(min, max, decimalPlaces) {
   return (Math.random() * (max - min) + min).toFixed(decimalPlaces);
 }
 
-const getRandomIndex = (arr) => getRandomInteger(0, arr.length - 1);
+const getRandomArrayElement = (arr) => arr[getRandomInteger(0, arr.length - 1)];
 const transformImgNumber = (number) => number.toString().padStart(2, '0');
 
 function getRandomLengthArray(arr) {
@@ -30,13 +30,4 @@ function getRandomLengthArray(arr) {
   return someValues;
 }
 
-function createCounter() {
-  let count = 0;
-
-  return function () {
-    count++;
-    return count;
-  };
-}
-
-export { getRandomInteger, getRandomFractNumber, getRandomIndex, transformImgNumber, getRandomLengthArray, createCounter };
+export { getRandomInteger, getRandomFractNumber, getRandomArrayElement, transformImgNumber, getRandomLengthArray };
