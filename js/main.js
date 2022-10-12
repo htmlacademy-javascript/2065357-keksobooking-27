54211b13c210a20066441fb0f00c8757747ebeca
Vanesflat <ptptcptptc@yandex.ru>
2022-10-13 00:24:18 +0700

    Added form validation

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 import { switchToActiveState } from '../js/user-form.js';
 import { adListFragment } from '../js/popup.js';
 
-switchToActiveState();
 const canvas = document.querySelector('#map-canvas');
 canvas.append(adListFragment.firstChild);
+
+switchToActiveState();
