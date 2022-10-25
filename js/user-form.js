53a541c12dffd2c2011604c69e6983ef6789dbe8
Vanesflat <ptptcptptc@mail.ru>
2022-10-25 11:44:34 +0700

    Minor updates

--- js/user-form.js
+++ js/user-form.js
@@ -1,6 +1,11 @@
-const adForm = document.querySelector('.ad-form');
+import { sendData } from './api.js';
+import { updateMainMarker } from './map.js';
+import { adForm, adFormSlider, pristine } from './validate.js';
+
 const mapForm = document.querySelector('.map__filters');
 const fieldsOfForms = document.querySelectorAll('select.map__filter, fieldset');
+const submitButton = adForm.querySelector('.ad-form__submit');
+const resetButton = adForm.querySelector('.ad-form__reset');
 
 const switchStateElements = () => {
   fieldsOfForms.forEach((element) => {
@@ -15,4 +20,47 @@ const switchPageMode = () => {
   switchStateElements();
 };
 
-export { switchPageMode, adForm };
+
+const blockSubmitButton = () => {
+  submitButton.disabled = true;
+  submitButton.textContent = 'Публикация...';
+};
+
+const unblockSubmitButton = () => {
+  submitButton.disabled = false;
+  submitButton.textContent = 'Опубликовать';
+};
+
+const resetForm = (form) => {
+  adFormSlider.noUiSlider.set(0);
+  form.reset();
+  updateMainMarker();
+};
+
+resetButton.addEventListener('click', (evt) => {
+  evt.preventDefault();
+  resetForm(adForm);
+});
+
+const setAdFormSubmit = (onSuccess, onError) => {
+  adForm.addEventListener('submit', (evt) => {
+    evt.preventDefault();
+    if (pristine.validate()) {
+      blockSubmitButton();
+      sendData(
+        () => {
+          onSuccess();
+          resetForm(evt.target);
+          unblockSubmitButton();
+        },
+        () => {
+          onError();
+          unblockSubmitButton();
+        },
+        new FormData(evt.target)
+      );
+    }
+  });
+};
+
+export { setAdFormSubmit, switchPageMode };
