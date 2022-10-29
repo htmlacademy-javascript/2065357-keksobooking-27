729218e7fbe732302e9da844d3f5d19b233ee4a2
Vanesflat <ptptcptptc@yandex.ru>
2022-10-29 19:08:16 +0700

    Fix

--- js/user-form.js
+++ js/user-form.js
@@ -1,4 +1,4 @@
-import { sendData } from './api.js';
+import { request } from './api.js';
 import { resetFilters } from './filter.js';
 import { updateMainMarker } from './map.js';
 import { adForm, adFormSlider, pristine, setPricePlaceholder } from './validate.js';
@@ -63,7 +63,7 @@ const setAdFormSubmit = (onSuccess, onError) => {
     evt.preventDefault();
     if (pristine.validate()) {
       blockSubmitButton();
-      sendData(
+      request(
         () => {
           onSuccess();
           resetFilters();
@@ -74,6 +74,7 @@ const setAdFormSubmit = (onSuccess, onError) => {
           onError();
           unblockSubmitButton();
         },
+        'POST',
         new FormData(evt.target)
       );
     }
